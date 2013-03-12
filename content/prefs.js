5a10d712ee1ec0c0116b5c2d10929d72dab100ff
Gavin Lloyd <gavinhungry@gmail.com>
2013-03-12 08:59:14 -0700

    options cleanup

--- content/prefs.js
+++ content/prefs.js
@@ -2,15 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var CipherFox_prefs = {
+var CipherFox_prefs = (function() {
+  'use strict';
 
-  onLoad: function() {
-    var Cc = Components.classes;
-    var Ci = Components.interfaces;
-
-    this.prompt = Cc['@mozilla.org/embedcomp/prompt-service;1'].getService(Ci.nsIPromptService);
-    this.bundle = document.getElementById('cipherfox-prefs-bundle');
-    this.baseFormat = document.getElementById('pref_base_format');
-    this.certFormat = document.getElementById('pref_cert_format');
-  }
-};
+  return {
+    onLoad: function() {
+      this.bundle = document.getElementById('cipherfox-prefs-bundle');
+      this.baseFormat = document.getElementById('pref_base_format');
+      this.certFormat = document.getElementById('pref_cert_format');
+    }
+  };
+})();
