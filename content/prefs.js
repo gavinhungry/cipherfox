3ee72cc7b9fa79a5365cc890d356c3ef60927055
Gavin Lloyd <gavinhungry@gmail.com>
2013-03-26 14:03:13 -0700

    Revert "options cleanup"
    This reverts commit 5a10d712ee1ec0c0116b5c2d10929d72dab100ff.

--- content/prefs.js
+++ content/prefs.js
@@ -2,14 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var CipherFox_prefs = (function() {
-  'use strict';
+var CipherFox_prefs = {
 
-  return {
-    onLoad: function() {
-      this.bundle = document.getElementById('cipherfox-prefs-bundle');
-      this.baseFormat = document.getElementById('pref_base_format');
-      this.certFormat = document.getElementById('pref_cert_format');
-    }
-  };
-})();
+  onLoad: function() {
+    var Cc = Components.classes;
+    var Ci = Components.interfaces;
+
+    this.prompt = Cc['@mozilla.org/embedcomp/prompt-service;1'].getService(Ci.nsIPromptService);
+    this.bundle = document.getElementById('cipherfox-prefs-bundle');
+    this.baseFormat = document.getElementById('pref_base_format');
+    this.certFormat = document.getElementById('pref_cert_format');
+  }
+};
